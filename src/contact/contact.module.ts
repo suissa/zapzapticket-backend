7b6b6906455ee7f152e2941bf797b711b1d81c0c
Suissinha <jnascimento@gmail.com>
2023-12-22 17:36:55 -0300

    Contact - Module - created

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,13 +3,12 @@ import { ContactService } from './service/contact.service';
 import { ContactController } from './controller/contact.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Contact, ContactSchema } from './schema/contact.schema';
-import { EvolutionModule } from "../evolution/evolution.module"
-import { EvolutionService } from 'src/evolution/service/evolution.service';
+
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
   ],
   controllers: [ContactController],
-  providers: [ContactService, EvolutionService],
+  providers: [ContactService],
 })
 export class ContactModule {}
