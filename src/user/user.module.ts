efbe3a64265a00ef78fcf8d648135cdd8051d42f
Suissinha <jnascimento@gmail.com>
2023-12-22 17:37:46 -0300

    User - Module - created

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,6 +9,6 @@ import { User, UserSchema } from './schema/user.schema';
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService,],
+  providers: [UserService],
 })
 export class UserModule {}
