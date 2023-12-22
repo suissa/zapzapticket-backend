f1fc2185b5cb48c78a636175e0bef1ba0ca90f72
Suissinha <jnascimento@gmail.com>
2023-12-22 17:25:23 -0300

    DTO - User - create

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,78 @@
-import { IsNotEmpty, IsOptional, Min } from 'class-validator';
+import { IsString, IsNotEmpty, IsArray, IsOptional, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class CreateUserDto {
+class BadgeDto {
+  @IsString()
+  type: string;
+}
+
+class MessageDto {
+  @IsString()
+  @IsOptional()
+  type: string = 'text';
+
+  @IsString()
+  @IsOptional()
+  typeMessage: string = 'text';
+
+  @IsString()
+  text: string;
+
+  @IsString()
+  @IsOptional()
+  createdAt: Date;
+
+  @IsString()
+  @IsOptional()
+  phoneReply: string;
+}
+
+export class CreateContactDto {
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsNotEmpty()
+  @IsString()
   phone: string;
 
-  @IsOptional()
-  instanceName: string = "";
+  @IsNotEmpty()
+  @IsString()
+  status: string;
 
+  @IsString()
   @IsOptional()
-  instanceStatus: boolean = false;
+  city: string;
+
+  @IsString()
+  @IsOptional()
+  state: string;
+
+  @IsString()
+  @IsOptional()
+  country: string;
+
+  @IsString()
+  @IsOptional()
+  ticketStatus: string;
+
+  @IsString()
+  @IsOptional()
+  ticketCreatedAt: string;
+
+  @IsString()
+  @IsOptional()
+  ticketClosedAt: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => BadgeDto)
+  @IsOptional()
+  badges: BadgeDto[];
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => MessageDto)
+  @IsOptional()
+  messages: MessageDto[];
 }
