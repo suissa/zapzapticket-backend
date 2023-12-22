7849862a4e57d8d7ace073e23c53782d3baa9ea8
Suissinha <jnascimento@gmail.com>
2023-12-22 17:34:42 -0300

    App Modeule - Contact Module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EvolutionModule } from './evolution/evolution.module';
 import { ConnectionModule } from './connection/connection.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { ConnectionModule } from './connection/connection.module';
     }),
     EvolutionModule,
     ConnectionModule,
+    ContactModule,
   ],
   controllers: [AppController],
   providers: [AppService],
