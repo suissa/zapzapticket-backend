94e916b586a0da413f9d9f5fa2bc31d4bae07c5d
Suissinha <jnascimento@gmail.com>
2023-12-23 20:50:51 -0300

    Connection Update DTO only with instanceStatus

--- src/connection/dto/update-connection.dto.ts
+++ src/connection/dto/update-connection.dto.ts
@@ -1,14 +1,6 @@
 import { IsNotEmpty, IsOptional, Min } from 'class-validator';
 
 export class UpdateConnectionDto {
-  @IsNotEmpty()
-  name: string;
-
-  @IsNotEmpty()
-  phone: string;
-
-  @IsOptional()
-  instanceName: string;
 
   @IsOptional()
   instanceStatus: boolean;
