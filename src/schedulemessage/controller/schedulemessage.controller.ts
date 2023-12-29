17c482d28e0adc2fb2884b1ef85615cf5f4a3474
Suissinha <jnascimento@gmail.com>
2023-12-29 18:15:04 -0300

    ScheduleMessage Controller OK

--- src/schedulemessage/controller/schedulemessage.controller.ts
+++ src/schedulemessage/controller/schedulemessage.controller.ts
@@ -9,42 +9,42 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
-import { MessageService } from '../service/message.service';
-import { CreateMessageDto } from '../dto/create-message.dto';
-import { UpdateMessagetDto } from '../dto/update-message.dto';
+import { ScheduleMessageService } from '../service/schedulemessage.service';
+import { CreateScheduleMessageDto } from '../dto/create-schedulemessage.dto';
+import { UpdateScheduleMessagetDto } from '../dto/update-schedulemessage.dto';
 
-@Controller('messages')
-export class MessageController {
-  constructor(private readonly messageService: MessageService) {}
+@Controller('schedulemessages')
+export class ScheduleMessageController {
+  constructor(private readonly schedulemessageService: ScheduleMessageService) {}
 
   @Post()
   @HttpCode(HttpStatus.OK)
-  create(@Body() request: CreateMessageDto) {
-    return this.messageService.create(request);
+  create(@Body() request: CreateScheduleMessageDto) {
+    return this.schedulemessageService.create(request);
   }
 
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll() {
     console.log('findAll');
-    return this.messageService.findAll();
+    return this.schedulemessageService.findAll();
   }
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
-    return this.messageService.findOne(id);
+    return this.schedulemessageService.findOne(id);
   }
 
   @Patch(':id')
   @HttpCode(HttpStatus.OK)
-  update(@Param('id') id: string, @Body() request: UpdateMessagetDto) {
-    return this.messageService.update(id, request);
+  update(@Param('id') id: string, @Body() request: UpdateScheduleMessagetDto) {
+    return this.schedulemessageService.update(id, request);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.OK)
   delete(@Param('id') id: string) {
-    return this.messageService.delete(id);
+    return this.schedulemessageService.delete(id);
   }
 }
