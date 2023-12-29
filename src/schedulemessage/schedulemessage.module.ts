527768a7f820271da6de4c8f18760d45e9e9d5e3
Suissinha <jnascimento@gmail.com>
2023-12-29 19:23:35 -0300

    ScheduleMessage Module OK

--- src/schedulemessage/schedulemessage.module.ts
+++ src/schedulemessage/schedulemessage.module.ts
@@ -3,10 +3,12 @@ import { ScheduleMessageService } from './service/schedulemessage.service';
 import { ScheduleMessageController } from './controller/schedulemessage.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleMessage, ScheduleMessageSchema } from './schema/schedulemessage.schema';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ScheduleMessage.name, schema: ScheduleMessageSchema }]),
+    ScheduleModule.forRoot(),
   ],
   controllers: [ScheduleMessageController],
   providers: [ScheduleMessageService],
