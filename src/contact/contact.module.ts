b2fb049d2ed586bf894ac7b9e78fe9dc6c85d9b2
Suissinha <jnascimento@gmail.com>
2023-12-28 22:36:20 -0300

    Contact Module import ConnectionModule

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,10 +3,12 @@ import { ContactService } from './service/contact.service';
 import { ContactController } from './controller/contact.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Contact, ContactSchema } from './schema/contact.schema';
+import { ConnectionModule } from '../connection/connection.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
+    ConnectionModule
   ],
   controllers: [ContactController],
   providers: [ContactService],
