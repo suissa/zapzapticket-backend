7c8c5712d31eab655c63d28f8784f126ed3037f9
Suissinha <jnascimento@gmail.com>
2023-12-29 21:51:53 -0300

    ScheduleMessage Module import EvolutionModule

--- src/schedulemessage/schedulemessage.module.ts
+++ src/schedulemessage/schedulemessage.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ScheduleMessageService } from './service/schedulemessage.service';
-import { ScheduleMessageController } from './controller/schedulemessage.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ScheduleMessage, ScheduleMessageSchema } from './schema/schedulemessage.schema';
-import { ScheduleModule } from '@nestjs/schedule';
+import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ScheduleMessageService } from "./service/schedulemessage.service";
+import { ScheduleMessageController } from "./controller/schedulemessage.controller";
+import { ScheduleMessage, ScheduleMessageSchema } from "./schema/schedulemessage.schema";
+import { ContactModule } from "src/contact/contact.module";
+import { ConnectionModule } from "src/connection/connection.module";
+import { EvolutionModule } from "src/evolution/evolution.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ScheduleMessage.name, schema: ScheduleMessageSchema }]),
     ScheduleModule.forRoot(),
+    ContactModule,
+    ConnectionModule,
+    EvolutionModule
   ],
   controllers: [ScheduleMessageController],
   providers: [ScheduleMessageService],
