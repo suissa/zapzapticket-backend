6536a4827e520fa5d477f64e45568fbf9c0fd72f
Suissinha <jnascimento@gmail.com>
2023-12-31 16:54:32 -0300

    Evolution Module merged

--- src/evolution/evolution.module.ts
+++ src/evolution/evolution.module.ts
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { Module } from '@nestjs/common';
-import { ConnectionService } from '../connection/service/connection.service';
-import { EvolutionService } from './service/evolution.service';
-import { ContactService } from '../contact/service/contact.service';
-import { MessageService } from '../message/service/message.service';
-import { EvolutionController } from './controller/evolution.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Evolution, EvolutionSchema } from './schema/evolution.schema';
-=======
 import { Module } from "@nestjs/common";
 import { RabbitmqService } from "./service/rabbitmq.service";
 import { EvolutionService } from "./service/evolution.service";
@@ -17,7 +7,6 @@ import { Evolution, EvolutionSchema } from "./schema/evolution.schema";
 import { MessageGateway }  from "../gateways/message.gateway";
 import { ClientsModule, Transport } from "@nestjs/microservices";
 
->>>>>>> develop
 
 @Module({
   imports: [
