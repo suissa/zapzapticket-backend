4c2e79b30abc6c54c72462757565d823664d047c
Suissinha <jnascimento@gmail.com>
2023-12-31 20:27:51 -0300

    Task Controller with /actives route

--- src/task/controller/task.controller.ts
+++ src/task/controller/task.controller.ts
@@ -8,12 +8,12 @@ import {
   Param,
   Patch,
   Post,
-} from '@nestjs/common';
-import { TaskService } from '../service/task.service';
-import { CreateTaskDto } from '../dto/create-task.dto';
-import { UpdateTasktDto } from '../dto/update-task.dto';
+} from "@nestjs/common";
+import { TaskService } from "../service/task.service";
+import { CreateTaskDto } from "../dto/create-task.dto";
+import { UpdateTasktDto } from "../dto/update-task.dto";
 
-@Controller('tasks')
+@Controller("tasks")
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
@@ -26,25 +26,31 @@ export class TaskController {
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll() {
-    console.log('findAll');
+    console.log("findAll");
     return this.taskService.findAll();
   }
 
-  @Get(':id')
+  @Get("/actives")
   @HttpCode(HttpStatus.OK)
-  findOne(@Param('id') id: string) {
+  findOne(@Param("id") id: string) {
     return this.taskService.findOne(id);
   }
 
-  @Patch(':id')
+  @Get(":id")
   @HttpCode(HttpStatus.OK)
-  update(@Param('id') id: string, @Body() request: UpdateTasktDto) {
+  findOne(@Param("id") id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Patch(":id")
+  @HttpCode(HttpStatus.OK)
+  update(@Param("id") id: string, @Body() request: UpdateTasktDto) {
     return this.taskService.update(id, request);
   }
 
-  @Delete(':id')
+  @Delete(":id")
   @HttpCode(HttpStatus.OK)
-  delete(@Param('id') id: string) {
+  delete(@Param("id") id: string) {
     return this.taskService.delete(id);
   }
 }
